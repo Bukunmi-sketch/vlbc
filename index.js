2b0a46c4f60d1dd5a52f3d2b80d4a7d28e1d4086
Bukunmi Olarinde <58373089+Bukunmi-sketch@users.noreply.github.com>
2022-07-04 20:38:56 -0700

    created repo
    upload repo

---
+++ index.js
@@ -0,0 +1,11 @@
+const bars=document.querySelector(".bars");
+const ul=document.querySelector("header ul");
+bars.onclick=()=>{
+     ul.style.left="-100%"
+}
+/*
+function toggleit(menu){
+    menu.classList.toggle('show');
+}
+
+*/
